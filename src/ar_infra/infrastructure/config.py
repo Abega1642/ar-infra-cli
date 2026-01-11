9eab94a6ed152913bf73a2271945ace9efe9af36
Abega1642 <hei.abega.2@gmail.com>
2026-01-11 11:44:16 +0300

    refactor: update bot_id & bot_slug

--- src/ar_infra/infrastructure/config.py
+++ src/ar_infra/infrastructure/config.py
@@ -1,8 +1,8 @@
 import os
 
 
-BOT_ID = os.getenv("BOT_ID", "__BOT_ID_PLACEHOLDER__")
-BOT_SLUG = os.getenv("BOT_SLUG", "__BOT_SLUG_PLACEHOLDER__")
+BOT_ID = "__BOT_ID_PLACEHOLDER__"
+BOT_SLUG = "__BOT_SLUG_PLACEHOLDER__"
 
 GITHUB_TOKEN = os.getenv("GITHUB_TOKEN", None)
 
