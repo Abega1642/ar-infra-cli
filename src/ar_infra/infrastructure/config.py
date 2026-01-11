6a04dfe35c91ea73d3169e5c6ddce166bac405a3
Abega1642 <hei.abega.2@gmail.com>
2026-01-11 12:17:13 +0300

    test: add integration test to check on bot cred

--- src/ar_infra/infrastructure/config.py
+++ src/ar_infra/infrastructure/config.py
@@ -5,9 +5,3 @@ BOT_ID = "__BOT_ID_PLACEHOLDER__"
 BOT_SLUG = "__BOT_SLUG_PLACEHOLDER__"
 
 GITHUB_TOKEN = os.getenv("GITHUB_TOKEN", None)
-
-if BOT_ID == "__BOT_ID_PLACEHOLDER__":
-    BOT_ID = "123456789"
-
-if BOT_SLUG == "__BOT_SLUG_PLACEHOLDER__":
-    BOT_SLUG = "ar-infra-bot"
